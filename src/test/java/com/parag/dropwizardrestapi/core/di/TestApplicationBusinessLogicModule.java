f0fc4386aa7e5305f627ada0d9b8537d19c66a6f
Parag Ghosh <paragpratim@gmail.com>
2019-08-30 03:09:30 +0530

    Tests added.

---
+++ src/test/java/com/parag/dropwizardrestapi/core/di/TestApplicationBusinessLogicModule.java
@@ -0,0 +1,18 @@
+package com.parag.dropwizardrestapi.core.di;
+
+import org.mockito.Mockito;
+
+import com.parag.dropwizardrestapi.db.BaseDAO;
+import com.parag.dropwizardrestapi.db.impl.CustomerDAO;
+
+public class TestApplicationBusinessLogicModule extends ApplicationBusinessLogicModule {
+
+	@Override
+	protected void configure() {
+		super.configure();
+
+		// Bind DAOs for mock and make sure they are serializable.
+		bind(BaseDAO.class).toInstance(Mockito.mock(CustomerDAO.class, Mockito.withSettings().serializable()));
+	}
+
+}
--- src/test/java/com/parag/dropwizardrestapi/resources/impl/CustomerResourcesTest.java
+++ src/test/java/com/parag/dropwizardrestapi/resources/impl/CustomerResourcesTest.java
@@ -2,28 +2,39 @@ package com.parag.dropwizardrestapi.resources.impl;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.List;
 
+import javax.ws.rs.core.GenericType;
+
+import org.assertj.core.api.Assertions;
 import org.junit.ClassRule;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 
 import com.google.inject.Inject;
-import com.google.inject.matcher.Matchers;
+import com.parag.dropwizardrestapi.api.BaseDTO;
 import com.parag.dropwizardrestapi.api.impl.CustomerDTO;
+import com.parag.dropwizardrestapi.core.di.TestApplicationBusinessLogicModule;
 import com.parag.dropwizardrestapi.db.BaseDAO;
-
+import com.parag.dropwizardrestapi.core.testutils.GuiceJUnitRunner;
 import io.dropwizard.testing.junit.ResourceTestRule;
 
+@RunWith(GuiceJUnitRunner.class)
+@GuiceJUnitRunner.GuiceModules(TestApplicationBusinessLogicModule.class)
 public class CustomerResourcesTest {
 
 	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("YYYY-MM-DD");
-	private CustomerDTO aCustomer;
+	private BaseDTO aCustomer;
 
-	@InjectMocks
-	private static BaseDAO customerDAO;
+	@Inject
+	private BaseDAO customerDAO;
+
 	@Inject
 	private static CustomerResources customerResources;
 
@@ -33,8 +44,7 @@ public class CustomerResourcesTest {
 	@BeforeEach
 	private void setUp() throws ParseException {
 		aCustomer = new CustomerDTO(1222, "Parag", "Ghosh", "paragpratim@gmail.com", DATE_FORMAT.parse("2019-08-24"));
-		Mockito.when(customerDAO.get(Matchers.any()).thenReturn(aCustomer);
-		;
+		Mockito.when(customerDAO.get(ArgumentMatchers.anyLong())).thenReturn(aCustomer);
 	}
 
 	@AfterEach
@@ -43,9 +53,15 @@ public class CustomerResourcesTest {
 	}
 
 	@Test
-	public void getCustomerTest() {
-		
+	public void getAllCustomerTest() {
+		List<BaseDTO> expected = Collections.singletonList(aCustomer);
+		List<BaseDTO> actual = resources.target("/api/customers").request().get(new GenericType<List<BaseDTO>>() {
+		});
+
+		Assertions.assertThat(actual).containsAll(expected);
 
 	}
 
 }
+
+//
