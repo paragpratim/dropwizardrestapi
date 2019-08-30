6941bcaa06e9bc55c1462206961faebb5689e009
Parag Ghosh <paragpratim@gmail.com>
2019-08-30 13:36:22 +0530

    Changes Made.

--- src/test/java/com/parag/dropwizardrestapi/api/impl/CustomerDTOTest.java
+++ src/test/java/com/parag/dropwizardrestapi/api/impl/CustomerDTOTest.java
@@ -8,9 +8,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.dropwizard.jackson.Jackson;
@@ -19,18 +16,17 @@ import io.dropwizard.testing.FixtureHelpers;
 public class CustomerDTOTest {
 
 	private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
-	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("YYYY-MM-DD");
+	private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-DD");
 	private CustomerDTO aCustomer;
 
 	@BeforeEach
 	private void setUp() throws ParseException {
-		aCustomer = new CustomerDTO(1222, "Parag", "Ghosh", "paragpratim@gmail.com", DATE_FORMAT.parse("2019-08-24"));
-		;
+		aCustomer = new CustomerDTO(1222, "Parag", "Ghosh", "paragpratim@gmail.com", dateFormat.parse("2019-08-24"));
 	}
 
 	@Test
 	public void serializationTest()
-			throws ParseException, JsonParseException, JsonMappingException, JsonProcessingException, IOException {
+			throws IOException {
 		String expected = MAPPER.writeValueAsString(
 				MAPPER.readValue(FixtureHelpers.fixture("fixtures/customer.json"), CustomerDTO.class));
 		Assertions.assertEquals(MAPPER.writeValueAsString(aCustomer), expected);
@@ -38,7 +34,7 @@ public class CustomerDTOTest {
 
 	@Test
 	public void deserializationTest()
-			throws ParseException, JsonParseException, JsonMappingException, JsonProcessingException, IOException {
+			throws IOException {
 		CustomerDTO actual = MAPPER.readValue(FixtureHelpers.fixture("fixtures/customer.json"), CustomerDTO.class);
 		Assertions.assertEquals(aCustomer.toString(), actual.toString());
 	}
